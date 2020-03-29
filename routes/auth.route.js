56ed8eee0820f00b7214a45323c265cd628df379
mohammadharbi <mohammad.a.aljagthmi@gmail.com>
2020-03-29 11:51:36 +0300

    auth route is updated

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,7 +5,25 @@ const User = require("../models/user.model");
 const List = require("../models/list.model");
 const Book = require("../models/book.model")
 
-
+router.get("/auth/signup", (request, response) => {
+    response.render("auth/signup");
+  });
+  
+  router.post("/auth/signup", (request, response) => {
+    let user = new User(request.body);
+    
+    user
+      .save()
+      .then(() => {
+        passport.authenticate("local", {
+          successRedirect: "/bookpage", //after login success
+          successFlash: "You have logged In!"
+        })(request, response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 
 
 module.exports = router;

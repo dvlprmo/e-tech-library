34388ab766c8af7a5b59e3169dd9049c9381f1cd
Ghadeer Alwafi <57371668+ghadeera92@users.noreply.github.com>
2020-04-01 16:15:27 +0300

    Check Style for all pages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const flash = require("connect-flash");
 const passport = require("./config/passportConfig");
 const expressLayouts = require("express-ejs-layouts");
 const authRoutes = require("./routes/auth.route");
-const MongoStore = require('connect-mongo')(session); 
+const MongoStore = require('connect-mongo')(session);
 // connecting database with the server
 mongoose.connect(
     process.env.MONGODB,
